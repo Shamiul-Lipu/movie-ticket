e449919c897fdce97b2facab5822d9858ba6b985
unknown <shamiullipu@gmail.com>
2023-06-01 19:12:04 +0600

    starting the project

---
+++ movie-ticket-client/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from 'react-bootstrap/Button';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <Button>React Bootstrap</Button>
+    </>
+  )
+}
+
+export default App
